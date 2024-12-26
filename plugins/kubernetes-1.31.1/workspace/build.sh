c1fc0fa9c76aa542174516add80cd5ccc9000d70
shupiaoyang <shupiaoyang1@huawei.com>
2024-12-26 19:39:35 +0800

    修正license信息

--- plugins/kubernetes-1.31.1/workspace/build.sh
+++ plugins/kubernetes-1.31.1/workspace/build.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
-# SPDX-License-Identifier: MulanPSL-2.0+
-# Copyright (c) 2020 Huawei Technologies Co., Ltd. All rights reserved.
+# -*- coding: utf-8 -*-
+# Copyright (c) 2024 Huawei Technologies Co., Ltd.
+# oeDeploy is licensed under the Mulan PSL v2.
+# You can use this software according to the terms and conditions of the Mulan PSL v2.
+# You may obtain a copy of Mulan PSL v2 at:
+#     http://license.coscl.org.cn/MulanPSL2
+# THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY OR FIT FOR A PARTICULAR
+# PURPOSE.
+# See the Mulan PSL v2 for more details.
+# Create: 2024-12-23
+# ======================================================================================================================
 
 set -e
 
