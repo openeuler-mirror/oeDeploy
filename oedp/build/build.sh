8db9ca7c3e595750a34271edd554dd1762f4a140
liujiangbin11 <jiangpancjy@163.com>
2025-02-21 15:59:45 +0800

    chore&&doc: 取消在安装RPM包时安装 python 库 tabulate 的操作，并修改构建指南.md

--- oedp/build/build.sh
+++ oedp/build/build.sh
@@ -19,88 +19,28 @@ if [ ! -z $1 ]; then
     exec >> "$1" 2>&1
 fi
 
-function prepare_packing_dir() {
-    # 创建打包目录并复制文件
-    echo -e "\e[1;32m [INFO] Start to prepare packing directory \e[0m"
-    cd "${PROJECT_DIR}"
-    [ -d "${PACKING_DIR}" ] && rm -rf "${PACKING_DIR}"
-    mkdir "${PACKING_DIR}"
-    cp -R src oedp.py build/static "${PYTHON_THIRD_LIBS_DIR}" "${PACKING_DIR}"
-    echo -e "\e[1;32m [INFO] The packing directory si ready \e[0m"
-}
+echo -e "\e[1;32m [INFO] Start to build RPM \e[0m"
+[ -d ~/rpmbuild ] && rm -rf ~/rpmbuild
+rpmdev-setuptree
+cp "${BUILD_SCRIPT_DIR}"/oedp.spec ~/rpmbuild/SPECS/
+cp "${TEMP_DIR}/${PACKING_DIR_NAME}.tar.gz" ~/rpmbuild/SOURCES/
+sed -i "s/release_number/$(date +'%Y%m%d')/g" ~/rpmbuild/SPECS/oedp.spec
+sed -i '/case / s/^\(.*\)$/#\1/g' ~/.rpmmacros
+# 在构建 RPM 包时，禁用编译 pyc
+second_line=$(sed -n '2{s/^[[:space:]]*//;s/[[:space:]]*$//;p}' /usr/lib/rpm/brp-python-bytecompile)
+[ "$second_line" != "exit 0" ] && sudo sed -i '1 a exit 0' /usr/lib/rpm/brp-python-bytecompile
+rpmbuild -bb ~/rpmbuild/SPECS/oedp.spec
+echo -e "\e[1;32m [INFO] oedp built successfully \e[0m"
 
-function dos_2_unix() {
-    echo -e "\e[1;32m [INFO] Start to convert a newline character \e[0m"
-    find "${PACKING_DIR}" -type f -name "*.py" -o -name "*.sh" | xargs dos2unix
-    dos2unix "${BUILD_SCRIPT_DIR}"/oedp.spec
-    echo -e "\e[1;32m [INFO] Convert a newline character successfully \e[0m"
-}
+echo -e "\e[1;32m [INFO] Start to store RPMs \e[0m"
+cd "${WORKSPACE_DIR}"
+[ ! -d "${STORAGE_DIR}" ] && mkdir "${STORAGE_DIR}"
+sub_dir=$(date +'%Y%m%d%H%M')
+mkdir "${STORAGE_DIR}/${sub_dir}"
+cp -f ~/rpmbuild/RPMS/$(arch)/*.rpm "${STORAGE_DIR}/${sub_dir}"
+rpm_name=$(ls ~/rpmbuild/RPMS/$(arch))
+echo -e "\e[1;32m [INFO] RPM is stored in ${STORAGE_DIR}/${sub_dir}/${rpm_name} \e[0m"
 
-function pack_dir() {
-    echo -e "\e[1;32m [INFO] Start to pack source code \e[0m"
-    cd "${TEMP_DIR}"
-    [ -f "${PACKING_DIR_NAME}.tar.gz" ] && rm -f "${PACKING_DIR_NAME}.tar.gz"
-    tar czf "${PACKING_DIR_NAME}.tar.gz" "${PACKING_DIR_NAME}"
-    echo -e "\e[1;32m [INFO] Source code packed successfully \e[0m"
-}
-
-function build_rpm() {
-    echo -e "\e[1;32m [INFO] Start to build RPM \e[0m"
-    [ -d ~/rpmbuild ] && rm -rf ~/rpmbuild
-    rpmdev-setuptree
-    cp "${BUILD_SCRIPT_DIR}"/oedp.spec ~/rpmbuild/SPECS/
-    cp "${TEMP_DIR}/${PACKING_DIR_NAME}.tar.gz" ~/rpmbuild/SOURCES/
-    sed -i "s/release_number/$(date +'%Y%m%d')/g" ~/rpmbuild/SPECS/oedp.spec
-    sed -i '/case / s/^\(.*\)$/#\1/g' ~/.rpmmacros
-    # 在构建 RPM 包时，禁用编译 pyc
-    sudo sed -i '1 a exit 0' /usr/lib/rpm/brp-python-bytecompile
-    sudo sed -i '1 a exit 0' /usr/lib/rpm/check-buildroot
-    sudo sed -i '1 a exit 0' /usr/lib/rpm/check-rpaths
-    rpmbuild -bb ~/rpmbuild/SPECS/oedp.spec
-    echo -e "\e[1;32m [INFO] oedp built successfully \e[0m"
-}
-
-function archive_rpm() {
-    echo -e "\e[1;32m [INFO] Start to store RPMs \e[0m"
-    cd "${WORKSPACE_DIR}"
-    [ ! -d "${STORAGE_DIR}" ] && mkdir "${STORAGE_DIR}"
-    sub_dir=$(date +'%Y%m%d%H%M')
-    mkdir "${STORAGE_DIR}/${sub_dir}"
-    cp -f ~/rpmbuild/RPMS/$(arch)/*.rpm "${STORAGE_DIR}/${sub_dir}"
-    rpm_name=$(ls ~/rpmbuild/RPMS/$(arch))
-    echo -e "\e[1;32m [INFO] RPM is stored in ${STORAGE_DIR}/${sub_dir}/${rpm_name} \e[0m"
-}
-
-function clean_env() {
-    echo -e "\e[1;32m [INFO] Start to clean up environment \e[0m"
-    [ -d "${PACKING_DIR}" ] && rm -rf "${PACKING_DIR}"
-    echo -e "\e[1;32m [INFO] Clean up environment successfully \e[0m"
-}
-
-function main() {
-    # 检查暂存目录
-    if [ ! -d "${TEMP_DIR}" ]; then
-        mkdir "${TEMP_DIR}"
-        echo -e "\e[1;32m [INFO] Create a staging directory ${TEMP_DIR} \e[0m"
-    fi
-
-    # 创建打包目录
-    prepare_packing_dir
-
-    # 转换换行符
-    dos_2_unix
-
-    # 打压缩包
-    pack_dir
-
-    # 构建 RPM 包
-    build_rpm
-
-    # RPM 包归档
-    archive_rpm
-
-    # 环境清理
-    clean_env
-}
-
-main
+echo -e "\e[1;32m [INFO] Start to clean up environment \e[0m"
+[ -d "${PACKING_DIR}" ] && rm -rf "${PACKING_DIR}"
+echo -e "\e[1;32m [INFO] Clean up environment successfully \e[0m"
