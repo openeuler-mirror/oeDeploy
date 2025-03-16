cb5aea46d06b5f35f2fbf36a5229fdad767fd132
Dingjiahui <dingjiahui4@huawei.com>
2025-03-16 23:38:01 +0800

    新增debug模式，允许跳过特定调试任务。重构deepseek-r1部署流程，将安装流程拆分为环境准备、安装、配置、启动四个独立步骤

--- oedp/src/commands/run/run_cmd.py
+++ oedp/src/commands/run/run_cmd.py
@@ -11,22 +11,27 @@
 # Create: 2024-12-23
 # ======================================================================================================================
 
+import time
+
 from src.commands.run.run_action import RunAction
 from src.exceptions.config_exception import ConfigException
 from src.utils.log.logger_generator import LoggerGenerator
 from src.utils.main_reader import MainReader
+from decimal import Decimal
 
 
 class RunCmd:
-    def __init__(self, action: str, project: str):
+    def __init__(self, action: str, project: str, debug: bool):
         """
         执行一个项目中的方法。
 
         :param action: 方法名称
         :param project: 项目目录路径
+        :param debug: 是否启用调试模式
         """
         self.action = action
         self.project = project
+        self.debug = debug
         self.log = LoggerGenerator().get_logger('run_cmd')
 
     def run(self):
@@ -35,15 +40,25 @@ class RunCmd:
 
         :return: 是否执行成功
         """
-        self.log.debug(f'Running cmd run: action={self.action}, project={self.project}')
+        start_time = time.time()
+        self.log.info(f'Start time: {time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(start_time))}')
         try:
-            main = MainReader(self.project)
-            action = main.get_action_detail(self.action)
-        except ConfigException as e:
-            self.log.error(f'Failed to get project main info: {e}')
-            return False
-        if 'tasks' not in action:
-            self.log.error(f'Failed to get tasks info: {action}')
-            return False
-        tasks = action['tasks']
-        return RunAction(self.project, self.action, tasks).run()
+            self.log.debug(f'Running cmd run: action={self.action}, project={self.project}')
+            try:
+                main = MainReader(self.project)
+                action = main.get_action_detail(self.action)
+            except ConfigException as e:
+                self.log.error(f'Failed to get project main info: {e}')
+                return False
+            if 'tasks' not in action:
+                self.log.error(f'Failed to get tasks info: {action}')
+                return False
+            tasks = action['tasks']
+            return RunAction(self.action, tasks, self.project, self.debug).run()
+        finally:
+            end_time = time.time()
+            seconds = Decimal(f"{format(end_time - start_time, '.1f')}")
+            formatted_time = time.strftime("%H:%M:%S", time.gmtime(float(seconds)))
+            elapsed_time = f"{formatted_time}.{int((seconds % 1) * 10)}"
+            self.log.info(f'End time: {time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(end_time))}')
+            self.log.info(f'Elapsed time: {elapsed_time}')
